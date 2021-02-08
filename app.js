bb2e6dffdc32406343d0856e3c362276cabb803b
shakibahammed <binshakibahammed2020@gmail.com>
2021-02-08 22:51:03 +0600

    complete assignment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// search Button Part 
 const buttonClick = ()=>{
     const search_text =document.getElementById("searchFild").value;
     const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${search_text}`
@@ -9,21 +10,46 @@ const buttonClick = ()=>{
 const mealDisplay = foodItems =>{
     const foodFild =document.getElementById("FoodDetails");
     foodItems.forEach( foodItem => {
-console.log(foodItem)
+
         const foodImg =document.createElement("div");
         foodImg.className = 'itemName'
 
         const foodName =document.createElement("h3");
         foodImg.innerHTML = `
-         <img src="${foodItem.strMealThumb}" >
+         <img onclick="ingredientPart(${foodItem.idMeal}) " src="${foodItem.strMealThumb}" >
          <h3>${foodItem.strMeal} </h3>
 
-         <button>Detials</button>
         `
         foodImg.appendChild(foodName);
          FoodDetails.appendChild(foodImg);
    
     })
-     
 
+   
 }
+
+// ingredientPart solving
+const ingredientPart =(id) => {
+const ingredientApi =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+fetch(ingredientApi)
+.then (res => res.json())
+.then (data => { 
+    const meal = data.meals[0];
+
+    document.getElementById("ingredientPart").innerHTML=  `
+    <img class="show-image" src="${meal.strMealThumb}" >
+    <h3>${meal.strMeal} </h3>
+    <h4> ${meal.strIngredient1}</h4>
+    <h4> ${meal.strIngredient2}</h4>
+    <h4> ${meal.strIngredient3}</h4>
+    <h4> ${meal.strIngredient4}</h4>
+    <h4> ${meal.strIngredient5}</h4>
+    <h4> ${meal.strIngredient6}</h4>
+    <h4> ${meal.strIngredient7}</h4>
+    <h4> ${meal.strIngredient8}</h4>
+    <h4> ${meal.strIngredient9}</h4>
+    `
+})
+
+}   
+
